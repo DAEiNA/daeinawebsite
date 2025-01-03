648793a81c9f2defc44bdb1d9aae17ec0d06da63
sebastianroelsgaard <daeina2012@gmail.com>
2025-01-03 14:21:57 +0100

    Removed redundant data.

--- OLDSITE/About_files/About.js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 2.0.4 local-build-20150315
-
-setTransparentGifURL('Media/transparent.gif');function applyEffects()
-{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
-function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('About_files/AboutMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');adjustLineHeightIfTooBig('id8');adjustFontSizeIfTooBig('id8');adjustLineHeightIfTooBig('id9');adjustFontSizeIfTooBig('id9');fixAllIEPNGs('Media/transparent.gif');IMpreload('About_files','shapeimage_1','0');IMpreload('About_files','shapeimage_1','1');IMpreload('About_files','shapeimage_1','2');IMpreload('About_files','shapeimage_1','3');IMpreload('About_files','shapeimage_1','4');applyEffects()}
